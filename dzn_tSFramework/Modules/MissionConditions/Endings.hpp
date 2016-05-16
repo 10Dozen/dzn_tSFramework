82df887e5fac2f5b268b1515751c03bac6f22593
10Dozen <steel.mjr@gmail.com>
2016-05-16 16:16:04 +0300

    Create Endings.hpp

---
+++ dzn_tSFramework/Modules/MissionConditions/Endings.hpp
@@ -0,0 +1,15 @@
+class CfgDebriefing
+{  
+    class Win
+    {
+        title = "Задание выполнено";
+        subtitle = "Успех!";
+        description = "Задача выполнена";
+    };                  
+    class Lose
+    {
+        title = "Задание провалено";
+        subtitle = "Провал!";
+        description = "Задача не была выполнена";
+    };
+};`
