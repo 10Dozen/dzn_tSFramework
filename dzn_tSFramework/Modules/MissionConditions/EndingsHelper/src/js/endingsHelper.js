2c1b314120f6879ba56c6a959dc47818a217d36c
eugene.tretyak <steel.mjr@gmail.com>
2021-04-20 17:26:38 +0300

    Fix Ending Helper multiline description export
    Fixes #95

--- dzn_tSFramework/Modules/MissionConditions/EndingsHelper/src/js/endingsHelper.js
+++ dzn_tSFramework/Modules/MissionConditions/EndingsHelper/src/js/endingsHelper.js
@@ -21,31 +21,35 @@ var textAreaSettings = {
 	,"width": "800px"
 }
 
-/*		
+/*
  *	COMMON FUNCTIONS
-*/ 
+*/
 
 function escapeQuotes(str) {
 	return ( str.replace(new RegExp('"','g'),'""') );
 };
 
-function getCode() {	
+function replaceNewline(str) {
+	return str.replace(/(?:\r\n|\r|\n)/g, '<br/>');
+}
+
+function getCode() {
 	var debriefing;
 	var debriefingBlockStart = "class CfgDebriefing\n{";
 	var debriefingBlockEnd = "\n};";
-	
+
 	var debriefingClasses = "";
 	for (var i = 0; i < Endings.length; i++) {
 		var Ending = Endings[i];
-		
+
 		var text = '\n	class ' + Ending.name
 			+ '\n	{'
 			+ '\n		title = "' + escapeQuotes(Ending.title) + '";'
 			+ '\n		subtitle = "' + escapeQuotes(Ending.subtitle) + '";'
-			+ '\n		description = "' + escapeQuotes(Ending.description) + '";'
+			+ '\n		description = "' + replaceNewline(escapeQuotes(Ending.description)) + '";'
 			+ '\n	};';
 
-		debriefingClasses = debriefingClasses + text;		
+		debriefingClasses = debriefingClasses + text;
 	};
 
 	debriefing = debriefingBlockStart + debriefingClasses + debriefingBlockEnd;
@@ -58,7 +62,7 @@ function getCodeToDisplay() {
 	code = code.replace(/</g, "&lt;");
 	code = code.replace(/>/g, "&gt;");
 	code = code.replace(/(\r\n|\n|\r)/g,"<br />");
-	
+
 	$( "#result-tab" ).css( "top", "15%" );
 	$( "#result-tab-data" ).html( code );
 }
@@ -75,19 +79,19 @@ function getCodeToFile() {
 }
 
 
-/*		
+/*
  *	ENDING FUNCTIONS
-*/ 
+*/
 var a = [];
 
 function getEndingById(id) {
 	var ending = {};
 	for (var i = 0; i < Endings.length; i++) {
-		if (Endings[i].id == id) { 
-			ending = Endings[i]; 
+		if (Endings[i].id == id) {
+			ending = Endings[i];
 			break;
 		}
-	};	
+	};
 	return ending;
 }
 
@@ -113,7 +117,7 @@ function checkDuplicateNames(name) {
 			break;
 		};
 	};
-	
+
 	if (name == "") { result = false };
 	return result;
 };
@@ -135,46 +139,46 @@ var Ending = function () {
 		+ "<li><div class='dl-1'>Subtitle</div><div class='dl-2'>"
 		+ "<input class='topicInput  ending-subtitle' placeholder='All mission objectives completed' value='" + this.subtitle + "'></input></div></li>"
 		+ "<li><div class='dl-1'>Description</div><div class='dl-3'>"
-		+ "<textarea class='topicData ending-desc' cols='" + textAreaSettings.cols 
+		+ "<textarea class='topicData ending-desc' cols='" + textAreaSettings.cols
 			+ "' rows='" + textAreaSettings.rows + "'></textarea>"
-		+ "</div></li><hr /></ul>"	
+		+ "</div></li><hr /></ul>"
 	);
-	
-	
+
+
 	this.setEndingName = function (newName) {
-		this.name = newName.replace(/[-[\]{}()*+?.,\\^$|#\s!@%=&]/g, "")	
+		this.name = newName.replace(/[-[\]{}()*+?.,\\^$|#\s!@%=&]/g, "")
 		$( this.$form ).find('.ending-name').html( this.name );
 		$( this.$form ).find('.ending-name').css("display", "inline-block");
 	};
 	this.draw = function () {
 		$( "#endings-form" ).append( this.$form );
 	}
-	
+
 	this.initEvents = function () {
-		$(this.$form).find('.ending-remove').on('click', function () {			
+		$(this.$form).find('.ending-remove').on('click', function () {
 			var self = getEndingById( $ ($(this).parents()[0]).attr("endingId")  );
 			self.remove();
 		});
-		
-		$(this.$form).find('.ending-name').on('click', function () {			
+
+		$(this.$form).find('.ending-name').on('click', function () {
 			var id = $( $(this).parents()[1] ).attr("endingId");
 			var name = $( this ).html();
 			$( this ).css( "display", "none" );
-			
+
 			var $inputName = $(this).parent().append(
 				"<div class='ending-name-input' endingId='" + id + "'>"
 				+ "<input value='" + name + "'/>"
 				+  "<span class='ending-name-btn ending-name-accept'>✓</span>"
 				+ "<span class='ending-name-btn ending-name-decline'>✗</span></div>"
-			);			
-			
+			);
+
 			$( $inputName ).find('.ending-name-btn').on('click', function () {
 				$( $( this ).parent() ).find( '.ending-name-accept' ).off();
 				$( $( this ).parent() ).find( '.ending-name-decline' ).off();
 				$( $( this ).parent() ).remove();
 			})
 			$( $inputName ).find('.ending-name-accept').on('click', function () {
-				var newName = $( this ).parent().find('input').val();				
+				var newName = $( this ).parent().find('input').val();
 				var ending = getEndingById( $(this).parent().attr("endingId")  );
 
 				ending.setEndingName(escapeDuplicateNames(ending.name, newName));
@@ -187,47 +191,47 @@ var Ending = function () {
 			$( $inputName ).find('.ending-name-decline').on('click', function () {
 				var ending = getEndingById( $(this).parent().attr("endingId")  );
 				ending.setEndingName(ending.name);
-			});			
+			});
 		});
-		
-		$(this.$form).find('.ending-title').on('blur', function () {			
+
+		$(this.$form).find('.ending-title').on('blur', function () {
 			var self = getEndingById( $ ($(this).parents()[2]).attr("endingId")  );
 			self.title = $( this ).val();
 		});
-		$(this.$form).find('.ending-subtitle').on('blur', function () {			
+		$(this.$form).find('.ending-subtitle').on('blur', function () {
 			var self = getEndingById( $ ($(this).parents()[2]).attr("endingId")  );
 			self.subtitle = $( this ).val();
 		});
-		$(this.$form).find('.ending-desc').on('blur', function () {			
+		$(this.$form).find('.ending-desc').on('blur', function () {
 			var self = getEndingById( $ ($(this).parents()[2]).attr("endingId")  );
 			self.description = $( this ).val();
 		});
 	}
-	
+
 	this.remove = function () {
-		Endings.splice( Endings.indexOf(this), 1 );			
-		
+		Endings.splice( Endings.indexOf(this), 1 );
+
 		$( this.$form ).find('.ending-remove').off();
 		$( this.$form ).find('.ending-name').off();
 		$( this.$form ).find('.ending-name-btn').off();
 		$( this.$form ).find('.ending-title').off();
 		$( this.$form ).find('.ending-subtitle').off();
 		$( this.$form ).find('.ending-desc').off();
-		
+
 		$( this.$form ).remove();
-		
+
 		console.log("Ending Removed");
 	}
-	
+
 	this.init = function () {
 		this.draw();
-		this.initEvents();	
-		
+		this.initEvents();
+
 		EndingsMaxId++;
 		Endings.push(this);
 	}
-	
-	this.init();	
+
+	this.init();
 }
 
 function addEnding() {
